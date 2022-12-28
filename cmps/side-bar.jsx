8e7273370a2c6eac75d47ca76164de686b936240
Boydem <noyemdahan@gmail.com>
2022-12-28 12:53:41 +0200

    sidebar dynamic

--- cmps/side-bar.jsx
+++ cmps/side-bar.jsx
@@ -1,6 +1,19 @@
+import { SideBarMail } from "../apps/mail/cmps/side-bar-mail.jsx"
+import { SideBarNote } from "../apps/note/cmps/side-bar-note.jsx"
+
+const { useParams, useLocation } = ReactRouterDOM
+const { useState, useEffect } = React
+
 export function SideBar() {
-  return (
-    <aside className='side-bar flex flex-column'>
+  const [sidebarLoc, setSidebarLoc] = useState("home")
+  const location = useLocation()
+
+  useEffect(() => {
+    setSidebarLoc(location.pathname)
+  }, [location.pathname])
+
+  function getHomeSideBar() {
+    return (
       <nav className='tools-nav'>
         <ul>
           <li>
@@ -23,6 +36,18 @@ export function SideBar() {
           </li>
         </ul>
       </nav>
-    </aside>
-  )
+    )
+  }
+  function sidebarFor() {
+    switch (location.pathname) {
+      case "/note":
+        return <SideBarNote />
+      case "/mail":
+        return <SideBarMail />
+      default:
+        return getHomeSideBar()
+    }
+  }
+
+  return <aside className='side-bar flex flex-column'>{sidebarFor()}</aside>
 }
