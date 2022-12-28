39efc7ef144bce72d6f6f96a422fa4cc5b199460
ForeseN <yagosik@gmail.com>
2022-12-28 12:31:44 +0200

    fixed nav

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,31 +1,31 @@
-import { SearchHeader } from "./search-header.jsx"
+import { SearchHeader } from './search-header.jsx'
 
 const { NavLink } = ReactRouterDOM
 
 export function AppHeader() {
-  return (
-    <header className='app-header'>
-      <h3>Appsus</h3>
-      <SearchHeader />
-      <nav className='main-nav'>
-        <ul className='nav-links clean-list flex align-center'>
-          <li key={"home"}>
-            <NavLink to='/' className='nav-link'>
-              Home
-            </NavLink>
-          </li>
-          <li key={"mail"}>
-            <NavLink to='/mail' className='nav-link'>
-              Maily
-            </NavLink>
-          </li>
-          <li key={"notes"}>
-            <NavLink to='/notes' className='nav-link'>
-              Keepy
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  )
+    return (
+        <header className='app-header'>
+            <h3>Appsus</h3>
+            <SearchHeader />
+            <nav className='main-nav'>
+                <ul className='nav-links clean-list flex align-center'>
+                    <li key={'home'}>
+                        <NavLink to='/' className='nav-link'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li key={'mail'}>
+                        <NavLink to='/mail' className='nav-link'>
+                            Maily
+                        </NavLink>
+                    </li>
+                    <li key={'note'}>
+                        <NavLink to='/note' className='nav-link'>
+                            Keepy
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
