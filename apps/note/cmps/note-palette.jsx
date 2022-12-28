1db804cc5b224e0e155ef5004758a60ddab9a942
ForeseN <yagosik@gmail.com>
2022-12-29 00:13:32 +0200

    fixed notes palette

--- apps/note/cmps/note-palette.jsx
+++ apps/note/cmps/note-palette.jsx
@@ -1,12 +1,15 @@
 export default function NotePalette({ isInPalette }) {
+    function onColor(color) {
+        console.log(color)
+    }
     return (
         <div className={`note-palette ${isInPalette ? 'open' : ''}`}>
-            <button className='btn btn-rnd-s'></button>
-            <button className='btn btn-rnd-s'></button>
-            <button className='btn btn-rnd-s'></button>
-            <button className='btn btn-rnd-s'></button>
-            <button className='btn btn-rnd-s'></button>
-            <button className='btn btn-rnd-s'></button>
+            <button className='btn btn-rnd-s' onClick={() => onColor('#3269ff')}></button>
+            <button className='btn btn-rnd-s' onClick={() => onColor('#ffd947')}></button>
+            <button className='btn btn-rnd-s' onClick={() => onColor('#0e121a')}></button>
+            <button className='btn btn-rnd-s' onClick={() => onColor('#ae3b76')}></button>
+            <button className='btn btn-rnd-s' onClick={() => onColor('#0beaaf')}></button>
+            <button className='btn btn-rnd-s' onClick={() => onColor('#fe7746')}></button>
         </div>
     )
 }
