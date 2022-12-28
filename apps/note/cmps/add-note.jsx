93b9ee3d2b7466c7d1d2da3b18764b3bd247fd41
ForeseN <yagosik@gmail.com>
2022-12-28 15:05:51 +0200

    polishing the main-input

--- apps/note/cmps/add-note.jsx
+++ apps/note/cmps/add-note.jsx
@@ -8,6 +8,7 @@ export default function AddNote({ setNotes }) {
     const inputRef = useRef(null)
     useOutsideAlerter(inputRef)
     // console.log(addNodeParams)
+    console.log(addNodeParams.info)
 
     function handleChange({ target }) {
         let { value, name: field, type } = target
@@ -45,6 +46,10 @@ export default function AddNote({ setNotes }) {
     }
 
     function addNote() {
+        // Clear State
+        setIsWriting(false)
+        setAddNodeParams(noteService.getDefaultNote())
+
         noteService.saveNote(addNodeParams).then(newNote => {
             setNotes(prev => [newNote, ...prev])
         })
@@ -62,15 +67,30 @@ export default function AddNote({ setNotes }) {
                     onChange={handleChange}
                 />
             )}
-            <input
-                type='text'
-                placeholder='Take a note...'
-                id='txt'
-                name='txt'
-                value={addNodeParams.txt}
-                onChange={handleChange}
-                onClick={() => setIsWriting(true)}
-            />
+            <div className='main-input'>
+                <input
+                    type='text'
+                    placeholder='Take a note...'
+                    id='txt'
+                    name='txt'
+                    value={addNodeParams.txt}
+                    onChange={handleChange}
+                    onClick={() => setIsWriting(true)}
+                />
+                {!isWriting && (
+                    <div className='inline-utils'>
+                        <button className='btn btn-rnd-s'>
+                            <i className='fa-solid fa-pencil'></i>
+                        </button>
+                        <button className='btn btn-rnd-s'>
+                            <i className='fa-solid fa-palette'></i>
+                        </button>
+                        <button className='btn btn-rnd-s'>
+                            <i className='fa-solid fa-location-dot'></i>
+                        </button>
+                    </div>
+                )}
+            </div>
             {isWriting && (
                 <div className='utils'>
                     <div className='btns'>
