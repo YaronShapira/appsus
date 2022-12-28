3c681915e08fc3ff236c49beb9720899a08631c3
ForeseN <yagosik@gmail.com>
2022-12-28 20:13:43 +0200

    added useless charkmark for notes

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -1,11 +1,20 @@
 import NotePreview from './note-preview.jsx'
 
-export function NoteList({ notes,deleteNote,setNotes }) {
+export function NoteList({ notes, deleteNote, setNotes }) {
+    
+
     if (!notes || !notes.length) return ''
     return (
         <div className='note-list'>
             {notes.map(note => {
-                return <NotePreview note={note} key={note.id} deleteNote={deleteNote} setNotes={setNotes} />
+                return (
+                    <NotePreview
+                        note={note}
+                        key={note.id}
+                        deleteNote={deleteNote}
+                        setNotes={setNotes}
+                    />
+                )
             })}
         </div>
     )
--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -5,19 +5,57 @@ const { useState } = React
 
 export default function NotePreview({ note, deleteNote, setNotes }) {
     const [isEditing, setIsEditing] = useState(false)
+    const [isHovering, setIsHovering] = useState(false)
     function editNote() {
         console.log('F')
     }
+    function onDeleteNote(ev, id) {
+        ev.stopPropagation()
+        deleteNote(note.id)
+    }
+    function onHover(ev) {
+        console.log('ENTERING', ev.target)
+        setIsHovering(true)
+    }
+    function onHoverLeave(ev) {
+        console.log('LEAVING', ev.target)
+        setIsHovering(false)
+    }
+
     switch (note.type) {
         case 'note-txt':
             return (
                 <Fragment>
-                    <article className='note-preview' onClick={() => setIsEditing(true)}>
+                    <article
+                        className='note-preview'
+                        onClick={() => setIsEditing(true)}
+                        onMouseEnter={onHover}
+                        onMouseLeave={onHoverLeave}>
                         <h6>{note.title}</h6>
                         <p>{note.info.txt}</p>
-                        <button className='btn btn-primary' onClick={() => deleteNote(note.id)}>
-                            DELETE
-                        </button>
+                        {isHovering && (
+                            <Fragment>
+                                <div className='select'>
+                                    <button className='btn btn-rnd-s'>
+                                        <i className='fa-solid fa-check'></i>
+                                    </button>
+                                </div>
+                                <div className='utils'>
+                                    <button className='btn'>
+                                        <i className='fa-solid fa-trash'></i>
+                                    </button>
+                                    <button className='btn'>
+                                        <i className='fa-solid fa-palette'></i>
+                                    </button>
+                                    <button className='btn'>
+                                        <i className='fa-solid fa-image'></i>
+                                    </button>
+                                    <button className='btn'>
+                                        <i className='fa-solid fa-envelope'></i>
+                                    </button>
+                                </div>
+                            </Fragment>
+                        )}
                     </article>
                     {isEditing && (
                         <Fragment>
