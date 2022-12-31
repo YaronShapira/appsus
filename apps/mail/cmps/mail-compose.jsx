433fb37af1d033d9dfb9a1477cda37d965083a4e
ForeseN <yagosik@gmail.com>
2022-12-31 22:38:45 +0200

    adding img support for mail

--- apps/mail/cmps/mail-compose.jsx
+++ apps/mail/cmps/mail-compose.jsx
@@ -1,158 +1,156 @@
 const { useState, useEffect, useRef } = React
 import { eventBusService } from '../../../services/event-bus.service.js'
+import { uploadService } from '../../../services/upload.service.js'
 import { mailService } from '../services/mail.service.js'
 
 export function MailCompose({ sendMail, draftMail }) {
-  const [newMailToEdit, setNewMailToEdit] = useState(mailService.getEmptyMail())
-  const [isExpandedMsgWindow, setIsExpandedMsgWindow] = useState(false)
-  const [isMailEdited, setIsMailEdited] = useState(false)
-  const [isMailMinimized, setIsMailMinimized] = useState(false)
+    const [newMailToEdit, setNewMailToEdit] = useState(mailService.getEmptyMail())
+    const [isExpandedMsgWindow, setIsExpandedMsgWindow] = useState(false)
+    const [isMailEdited, setIsMailEdited] = useState(false)
+    const [isMailMinimized, setIsMailMinimized] = useState(false)
+    const uploadImgInputRef = useRef(null)
 
-  function onMinimizeMsgWindow() {
-    setIsMailMinimized(!isMailMinimized)
-    setIsExpandedMsgWindow(false)
-  }
-
-  function onExpandMsgWindow() {
-    setIsExpandedMsgWindow(!isExpandedMsgWindow)
-    setIsMailMinimized(false)
-  }
-
-  function onBtnToggleCompose() {
-    setIsMailEdited(!isMailEdited)
-    setIsExpandedMsgWindow(false)
-    setIsMailMinimized(false)
-  }
-
-  function onDraftMail() {
-    if (newMailToEdit.to) {
-      draftMail(newMailToEdit)
-      setNewMailToEdit(mailService.getEmptyMail())
+    function onMinimizeMsgWindow() {
+        setIsMailMinimized(!isMailMinimized)
+        setIsExpandedMsgWindow(false)
     }
-    setIsMailEdited(!isMailEdited)
-    setIsExpandedMsgWindow(false)
-    setIsMailMinimized(false)
-  }
 
-  function onSendMail(ev) {
-    ev.preventDefault()
-    sendMail(newMailToEdit)
-    setIsMailEdited(!isMailEdited)
-    setNewMailToEdit(mailService.getEmptyMail())
-    setIsExpandedMsgWindow(false)
-  }
+    function onExpandMsgWindow() {
+        setIsExpandedMsgWindow(!isExpandedMsgWindow)
+        setIsMailMinimized(false)
+    }
 
-  function handleForm({ target }) {
-    let { type, name: field, value } = target
-    setNewMailToEdit((prevMailToEdit) => ({ ...prevMailToEdit, [field]: value }))
-  }
+    function onBtnToggleCompose() {
+        setIsMailEdited(!isMailEdited)
+        setIsExpandedMsgWindow(false)
+        setIsMailMinimized(false)
+    }
 
-  return (
-    <section
-      className={`mail-compose ${isMailEdited ? 'open' : ''} ${
-        isExpandedMsgWindow ? 'expand' : ''
-      }`}>
-      <button
-        title='Compose Mail'
-        onClick={onBtnToggleCompose}
-        className={`btn-rnd-l btn-mail-compose ${isMailEdited ? 'open' : ''} ${
-          isMailMinimized ? 'minimized' : ''
-        }`}>
-        <i className={`fa-solid fa-pencil`}></i>
-      </button>
-      <section
-        className={`compose-mail-form flex flex-column ${isMailEdited ? 'open' : ''} ${
-          isExpandedMsgWindow ? 'expand' : ''
-        }`}>
-        <form
-          onSubmit={onSendMail}
-          className={`flex flex-column ${isMailMinimized ? 'minimized' : ''} `}>
-          <section className='compose-mail-form-header flex align-center justify-between'>
-            <h5>{}New Mail</h5>
-            <div className='header-tools'>
-              <button
-                onClick={onMinimizeMsgWindow}
-                title='Minimize'
-                type='button'
-                className='btn-rnd-s'>
-                <i className='fa-solid fa-window-minimize'></i>
-              </button>
-              <button
-                onClick={onExpandMsgWindow}
-                title='Expand'
-                type='button'
-                className='btn-rnd-s'>
-                {isExpandedMsgWindow ? (
-                  <i class='fa-solid fa-down-left-and-up-right-to-center'></i>
-                ) : (
-                  <i className='fa-solid fa-up-right-and-down-left-from-center'></i>
-                )}
-              </button>
-              <button
+    function onDraftMail() {
+        if (newMailToEdit.to) {
+            draftMail(newMailToEdit)
+            setNewMailToEdit(mailService.getEmptyMail())
+        }
+        setIsMailEdited(!isMailEdited)
+        setIsExpandedMsgWindow(false)
+        setIsMailMinimized(false)
+    }
+
+    function onSendMail(ev) {
+        ev.preventDefault()
+        sendMail(newMailToEdit)
+        setIsMailEdited(!isMailEdited)
+        setNewMailToEdit(mailService.getEmptyMail())
+        setIsExpandedMsgWindow(false)
+    }
+
+    function handleForm({ target }) {
+        let { type, name: field, value } = target
+        setNewMailToEdit(prevMailToEdit => ({ ...prevMailToEdit, [field]: value }))
+    }
+
+    function onUploadImg(ev) {
+        console.log(ev.target.files[0])
+        uploadService.loadImageFromInput(ev, img => {
+            console.log(img.src)
+        })
+    }
+
+    return (
+        <section className={`mail-compose ${isMailEdited ? 'open' : ''} ${isExpandedMsgWindow ? 'expand' : ''}`}>
+            <button
+                title='Compose Mail'
                 onClick={onBtnToggleCompose}
-                title='Close'
-                type='button'
-                className='btn-rnd-s'>
-                <i className='fa-solid fa-xmark'></i>
-              </button>
-            </div>
-          </section>
-          <div className='fields-container'>
-            <div className='field-wrapper'>
-              <label htmlFor='to'>To</label>
-              <input
-                autoComplete='off'
-                value={newMailToEdit.to}
-                type='email'
-                name='to'
-                id='to'
-                onChange={handleForm}
-              />
-            </div>
-            <div className='field-wrapper'>
-              <label htmlFor='subject'>Subject</label>
-              <input
-                autoComplete='off'
-                value={newMailToEdit.subject}
-                type='text'
-                name='subject'
-                id='subject'
-                onChange={handleForm}
-              />
-            </div>
-
-            <textarea
-              value={newMailToEdit.body}
-              placeholder='Your message...'
-              name='body'
-              rows={15}
-              id='body'
-              onChange={handleForm}
-            />
-          </div>
-          <div className='compose-mail-form-footer flex align-center justify-between'>
-            <button className='btn-primary' title='Send' type='submit'>
-              Send
+                className={`btn-rnd-l btn-mail-compose ${isMailEdited ? 'open' : ''} ${
+                    isMailMinimized ? 'minimized' : ''
+                }`}>
+                <i className={`fa-solid fa-pencil`}></i>
             </button>
-            <section className='footer-tools'>
-              <button
-                title='Upload Image'
-                onClick={onBtnToggleCompose}
-                type='button'
-                className='btn-rnd-l-s'>
-                <i className='fa-solid fa-image'></i>
-              </button>
-              <button
-                title='Sign'
-                onClick={onBtnToggleCompose}
-                type='button'
-                className='btn-rnd-l-s'>
-                <i className='fa-solid fa-signature'></i>
-              </button>
+            <section
+                className={`compose-mail-form flex flex-column ${isMailEdited ? 'open' : ''} ${
+                    isExpandedMsgWindow ? 'expand' : ''
+                }`}>
+                <form onSubmit={onSendMail} className={`flex flex-column ${isMailMinimized ? 'minimized' : ''} `}>
+                    <section className='compose-mail-form-header flex align-center justify-between'>
+                        <h5>{}New Mail</h5>
+                        <div className='header-tools'>
+                            <button onClick={onMinimizeMsgWindow} title='Minimize' type='button' className='btn-rnd-s'>
+                                <i className='fa-solid fa-window-minimize'></i>
+                            </button>
+                            <button onClick={onExpandMsgWindow} title='Expand' type='button' className='btn-rnd-s'>
+                                {isExpandedMsgWindow ? (
+                                    <i class='fa-solid fa-down-left-and-up-right-to-center'></i>
+                                ) : (
+                                    <i className='fa-solid fa-up-right-and-down-left-from-center'></i>
+                                )}
+                            </button>
+                            <button onClick={onBtnToggleCompose} title='Close' type='button' className='btn-rnd-s'>
+                                <i className='fa-solid fa-xmark'></i>
+                            </button>
+                        </div>
+                    </section>
+                    <div className='fields-container'>
+                        <div className='field-wrapper'>
+                            <label htmlFor='to'>To</label>
+                            <input
+                                autoComplete='off'
+                                value={newMailToEdit.to}
+                                type='email'
+                                name='to'
+                                id='to'
+                                onChange={handleForm}
+                            />
+                        </div>
+                        <div className='field-wrapper'>
+                            <label htmlFor='subject'>Subject</label>
+                            <input
+                                autoComplete='off'
+                                value={newMailToEdit.subject}
+                                type='text'
+                                name='subject'
+                                id='subject'
+                                onChange={handleForm}
+                            />
+                        </div>
+
+                        <textarea
+                            value={newMailToEdit.body}
+                            placeholder='Your message...'
+                            name='body'
+                            rows={15}
+                            id='body'
+                            onChange={handleForm}
+                        />
+
+                        <input
+                            type='file'
+                            className='file-input btn'
+                            name='image'
+                            id='image'
+                            hidden
+                            ref={uploadImgInputRef}
+                            onChange={onUploadImg}
+                        />
+                    </div>
+                    <div className='compose-mail-form-footer flex align-center justify-between'>
+                        <button className='btn-primary' title='Send' type='submit'>
+                            Send
+                        </button>
+                        <section className='footer-tools'>
+                            <button
+                                title='Upload Image'
+                                onClick={() => uploadImgInputRef.current.click()}
+                                type='button'
+                                className='btn-rnd-l-s'>
+                                <i className='fa-solid fa-image'></i>
+                            </button>
+                            <button title='Sign' onClick={onBtnToggleCompose} type='button' className='btn-rnd-l-s'>
+                                <i className='fa-solid fa-signature'></i>
+                            </button>
+                        </section>
+                    </div>
+                </form>
             </section>
-          </div>
-        </form>
-      </section>
-    </section>
-  )
+        </section>
+    )
 }
