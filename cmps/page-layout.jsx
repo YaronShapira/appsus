246880349d5e29760420212e42036bfae7590e4d
Boydem <noyemdahan@gmail.com>
2022-12-31 22:24:30 +0200

    user-msg added

--- cmps/page-layout.jsx
+++ cmps/page-layout.jsx
@@ -1,5 +1,6 @@
 import { AppHeader } from './app-header.jsx'
 import { SideBar } from './side-bar.jsx'
+import UserMsg from './user-msg.jsx'
 const { Fragment } = React
 
 export const PageLayout = ({ children }) => {
@@ -8,6 +9,7 @@ export const PageLayout = ({ children }) => {
       <AppHeader />
       <SideBar />
       <div className='overflow-wrapper'>{children}</div>
+      <UserMsg />
     </Fragment>
   )
 }
